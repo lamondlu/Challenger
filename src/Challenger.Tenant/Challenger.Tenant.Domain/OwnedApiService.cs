5d031e50fd3869c7340cb2b0e0a28afcc8f49fa6
lamondlu <309728709@qq.com>
2018-03-29 16:02:14 +0800

    add inital

---
+++ src/Challenger.Tenant/Challenger.Tenant.Domain/OwnedApiService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenger.Tenant.Domain
+{
+	public class OwnedApiService
+	{
+		public Guid OwnedApiServiceId { get; set; }
+
+		public ApiService ApiService { get; set; }
+
+		public string AppClientId { get; set; }
+
+		public string AppSecret { get; set; }
+	}
+}
---
+++ src/Challenger.Tenant/Challenger.Tenant.Domain/Tenant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenger.Tenant.Domain
+{
+	public class Tenant
+	{
+		public Guid TenantId { get; set; }
+
+		public string TenantName { get; set; }
+
+		public List<OwnedApiService> ApiServices { get; set; }
+	}
+}
